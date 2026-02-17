0ca0847eaea8f193fce0e25dccefb5e2894076d8
TejaMahindra Malleswarapu <109898193+tejamahindra@users.noreply.github.com>
2026-02-17 16:38:07 +0530

    Update videogamePlayer.ts

--- TypescriptProblems/videogamePlayer.ts
+++ TypescriptProblems/videogamePlayer.ts
@@ -1,33 +1,27 @@
-const player1: {
+type Rank = "Bronze" | "Silver" | "Gold" | "Platinum"
+
+type Player = {
   username: string
   level: number
   experiencePoints: number
-  rank: "Bronze" | "Silver" | "Gold" | "Platinum"
-} = {
+  rank: Rank
+}
+
+const player1: Player = {
   username: "ShadowHunter",
   level: 10,
   experiencePoints: 5000,
   rank: "Silver"
 }
 
-const player2: {
-  username: string
-  level: number
-  experiencePoints: number
-  rank: "Bronze" | "Silver" | "Gold" | "Platinum"
-} = {
+const player2: Player = {
   username: "DragonSlayer",
   level: 20,
   experiencePoints: 15000,
   rank: "Gold"
 }
 
-function promotePlayer(player: {
-  username: string
-  level: number
-  experiencePoints: number
-  rank: "Bronze" | "Silver" | "Gold" | "Platinum"
-}) {
+function promotePlayer(player: Player): Player {
   return {
     ...player,
     level: player.level + 1,
