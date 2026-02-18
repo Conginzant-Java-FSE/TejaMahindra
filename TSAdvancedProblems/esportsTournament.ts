ecef0398f778f6f7c3fac678b90f1e997eff2aea
TejaMahindra Malleswarapu <109898193+tejamahindra@users.noreply.github.com>
2026-02-18 11:12:34 +0530

    Create esportsTournament.ts

---
+++ TSAdvancedProblems/esportsTournament.ts
@@ -0,0 +1,23 @@
+interface Team {
+  readonly id: number
+  readonly name: string
+  players: string[]
+  rank: number
+}
+
+const team: Team = {
+  id: 101,
+  name: "CyberTitans",
+  players: ["Ryu", "Blaze", "Nova"],
+  rank: 5
+}
+
+function updateRank(team: Team, newRank: number): Team {
+  return {
+    ...team,
+    rank: newRank
+  }
+}
+
+console.log("Problem 1:", updateRank(team, 2))
+console.log("Original Team:", team)
